a6e4176dd89cc8899736d51dea79262016a5dc62
홍정화 <ekdan31@gmail.com>
2026-06-04 21:38:05 +0900

    222

---
+++ web/backend/src/test/java/com/sdi/aid/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdi.aid.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
